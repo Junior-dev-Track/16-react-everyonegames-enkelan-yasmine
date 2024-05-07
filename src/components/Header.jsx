bf4ceb39430a10de8954fadf8f6fffce6427883f
Eno <eno_rami@ymail.com>
2024-05-07 17:01:55 +0200

    changement du bar de nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
+import React from "react";
 import LogoGames from "../asset/images/Pandagame.svg";
-import { BiBasket } from "react-icons/bi";
 import { FaRegUserCircle } from "react-icons/fa";
 import { InputWithHover } from "./InputWithHover.jsx";
 import { GameProvider } from "./GameContext.jsx";
 import { Link } from "react-router-dom";
 
-
-
 const Header = () => {
-    return (
-        <header className='header'>
-            <nav>
-                <Link to={'/'} className={'logo'}>
-                    <img src={LogoGames} alt="Logo du site" className="LogoGames"/>
-                </Link>
-                <GameProvider>
-                    <InputWithHover/>
-                </GameProvider>
-                <div className="HeaderRight">
-                    <FaRegUserCircle className={'IconProfil'} style={{color: 'white'}} size={'35px'}/>
-                    <BiBasket className={'IconBasket'} style={{color: 'white'}} size={'35px'}/>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo">
+          <Link to={"/"}>
+            <img src={LogoGames} alt="Logo du site" className="LogoGames" />
+          </Link>
+        </div>
+        <GameProvider>
+          <div className="search-bar">
+            <InputWithHover />
+          </div>
+        </GameProvider>
+        <div className="HeaderRight">
+          <FaRegUserCircle
+            className={"IconProfil"}
+            style={{ color: "white" }}
+            size={"35px"}
+          />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
-
-export {Header};
+export { Header };
--- src/components/InputWithHover.jsx
+++ src/components/InputWithHover.jsx
@@ -1,70 +1,131 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function InputWithHover() {
-    const [isActive, setIsActive] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [games, setGames] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const API_KEY = import.meta.env.VITE_APP_RAWG_API_KEY;
+  const [isActive, setIsActive] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [games, setGames] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const API_KEY = import.meta.env.VITE_APP_RAWG_API_KEY;
 
-    const toggleSearch = () => {
-        setIsActive(!isActive);
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setIsActive(false);
+        setGames([]);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
     };
+  }, [searchRef]);
 
-    const handleSearch = async () => {
-        if (!searchTerm) return;
-        setIsLoading(true);
-        try {
-            const response = await axios.get(`https://api.rawg.io/api/games?search=${searchTerm}&key=${API_KEY}`);
-            setGames(response.data.results);
-            setIsLoading(false);
-        } catch (error) {
-            setError('Failed to fetch games');
-            setIsLoading(false);
-        }
+  useEffect(() => {
+    if (searchTerm) {
+      handleSearch();
+    }
+  }, [searchTerm]);
+
+  const toggleSearch = () => {
+    setIsActive(!isActive);
+  };
+
+  const [cache, setCache] = useState({});
+
+  const source = axios.CancelToken.source();
+
+  const handleSearch = async () => {
+    if (!searchTerm) return;
+    if (cache[searchTerm]) {
+      setGames(cache[searchTerm]);
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.rawg.io/api/games?search=${searchTerm}&key=${API_KEY}`,
+        { cancelToken: source.token },
+      );
+      setGames(response.data.results);
+      setCache((prevCache) => ({
+        ...prevCache,
+        [searchTerm]: response.data.results,
+      }));
+      setIsLoading(false);
+    } catch (error) {
+      if (axios.isCancel(error)) return;
+      setError("Failed to fetch games");
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchTerm) {
+        handleSearch();
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(delayDebounceFn);
+      source.cancel();
     };
+  }, [searchTerm]);
 
-    const handleClick = () => {
-        setSearchTerm('');
-    };
+  const handleClick = () => {
+    setSearchTerm("");
+    setIsActive(false);
+    setGames([]);
+  };
 
-    return (
-        <div className={"SearchBackground"}>
-            <div className={`SearchContainer ${isActive? 'active' : ''}`}>
-                <div className={'SearchIcon'}>
-                    <FaSearch className={'ScaleIcon'} size={'15px'} color={'#201F1F'} onClick={toggleSearch} />
-                </div>
-                <div className={'SearchInput'}>
-                    <input
-                        type="text"
-                        placeholder={'Enter search'}
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
-                    ></input>
-                    <span className={'SearchClear'} onClick={handleClick}></span>
-                </div>
-            </div>
-            {isLoading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            {games.length > 0 && (
-                <div className="SearchResults">
-                    {games.map(game => (
-                        <article className={'GameListResult'} key={game.id}>
-                            <Link to={`/game/${game.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                <h3>{game.name}</h3>
-                            </Link>
-                        </article>
-                    ))}
-                </div>
-            )}
+  return (
+    <div className={"SearchBackground"}>
+      <div
+        ref={searchRef}
+        className={`SearchContainer ${isActive ? "active" : ""}`}
+      >
+        <div className={"SearchIcon"}>
+          <FaSearch
+            className={"ScaleIcon"}
+            size={"15px"}
+            color={"#201F1F"}
+            onClick={toggleSearch}
+          />
         </div>
-    );
+        <div className={"SearchInput"}>
+          <input
+            type="text"
+            placeholder={"Enter search"}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && handleSearch()}
+          ></input>
+          <span className={"SearchClear"} onClick={handleClick}></span>
+        </div>
+      </div>
+      {games.length > 0 && (
+        <div className="SearchResults">
+          {games.map((game) => (
+            <article className={"GameListResult"} key={game.id}>
+              <Link
+                to={`/game/${game.id}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <h3>{game.name}</h3>
+              </Link>
+            </article>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export { InputWithHover };
