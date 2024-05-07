229f141c9ade994e76973168090841f2008a3fd2
yaz <yasmine-sanchez@proton.me>
2024-05-07 18:49:27 +0200

    Filters components Filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GlobalApi } from "./Services/GlobalApi.jsx";
 import "./styles/Index.scss";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { GameCarousel } from "./components/GameCarousel.jsx";
-import { GameDetailsPage } from "./pages/GameDetailsPage.jsx";
-import { GenreSelect } from "./components/GenreSelect.jsx";
-import { PlatformSelect } from "./components/PlatformSelect.jsx";
-import { SystemSelect } from "./components/SystemSelect.jsx";
-import { GameDetails } from "./components/GameDetails.jsx";
+import { GameDetailsPage } from './pages/GameDetailsPage.jsx';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/game/:id" element={<GameDetails />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <GameCarousel />
-                <GlobalApi />
-              </>
-            }
-          />
-        </Routes>
-        <div className={"FilterContainer"}>
-          <GenreSelect />
-          <PlatformSelect />
-          <SystemSelect />
-        </div>
-        <Footer />
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Header/>
+                <GameCarousel/>
+                <Routes>
+                    <Route path="/" element={<GlobalApi/>}/>
+                    <Route path="/game/:id" element={<GameDetailsPage/>}/>
+                </Routes>
+                <Footer/>
+            </Router>
+        </>
+    );
 }
 export default App;
