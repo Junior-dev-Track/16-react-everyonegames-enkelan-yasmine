58320dae5a37c9c92a3761a3d72c0a7113de0b7d
yaz <yasmine-sanchez@proton.me>
2024-05-07 18:50:54 +0200

    Filters components Filter

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,19 +1,10 @@
-import { Link } from 'react-router-dom';
 
 
-const SearchPage = ({ games }) => {
+
+const SearchPage = () => {
     return (
         <div>
-            <h1>Résultats de la recherche</h1>
-            {games.map(game => (
-                <div key={game.id}>
-                    <Link to={`/game/${game.id}`} style={{ color: 'white', textDecoration: 'none' }}>
-                        <h2>{game.name}</h2>
-                    </Link>
-                    <p>Date de sortie: {game.released}</p>
-                    <p>Genere: {game.genre}</p>
-                </div>
-            ))}
+
         </div>
     );
 };
