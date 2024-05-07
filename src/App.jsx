6c61c01ca2636da5654b7cc423ac2f981929948d
yaz <yasmine-sanchez@proton.me>
2024-05-07 18:52:29 +0200

    Filters components Filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GlobalApi } from "./Services/GlobalApi.jsx";
 import "./styles/Index.scss";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { GameCarousel } from "./components/GameCarousel.jsx";
-import { GameDetailsPage } from './pages/GameDetailsPage.jsx';
+import { GameDetailsPage } from "./pages/GameDetailsPage.jsx";
+import { GenreSelect } from "./components/GenreSelect.jsx";
+import { PlatformSelect } from "./components/PlatformSelect.jsx";
+import { SystemSelect } from "./components/SystemSelect.jsx";
+import { GameDetails } from "./components/GameDetails.jsx";
 
 function App() {
     return (
         <>
             <Router>
-                <Header/>
-                <GameCarousel/>
+                <Header />
                 <Routes>
-                    <Route path="/" element={<GlobalApi/>}/>
-                    <Route path="/game/:id" element={<GameDetailsPage/>}/>
+                    <Route path="/game/:id" element={<GameDetails />} />
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <GameCarousel />
+                                <GlobalApi />
+                            </>
+                        }
+                    />
                 </Routes>
-                <Footer/>
+                <div className={"FilterContainer"}>
+                    <GenreSelect />
+                    <PlatformSelect />
+                    <SystemSelect />
+                </div>
+                <Footer />
             </Router>
         </>
     );
